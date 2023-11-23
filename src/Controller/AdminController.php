c0b149b7972b69cc293e366ee76eeb49e063c5e2
Dan Roberts <74026141+danroberts108@users.noreply.github.com>
2023-11-23 11:53:31 +0000

    Completed roll changing for admin users.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserRoleType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,17 +27,45 @@ class AdminController extends AbstractController
 
         $users = $entityManager->getRepository(User::class)->findAll();
 
+        $thisuser = $entityManager->getRepository(User::class)->find($this->getUser())->getId() - 1;
+
+        unset($users[$thisuser]);
+
         return $this->render('/admin/user_roles.html.twig', [
             'users' => $users
         ]);
     }
 
-    #[Route('/admin/edit_user', name:'edit-user')]
-    public function editUser(EntityManagerInterface $entityManager, int $id) : Response {
+    #[Route('/admin/edit_user/{id}', name:'edit-user')]
+    public function editUser(EntityManagerInterface $entityManager, int $id, Request $request) : Response {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $user = $entityManager->getRepository(User::class)->find($id);
 
-        return $this->render('/admin/edit_user.html.twig');
+        $form = $this->createForm(UserRoleType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            if ($data['new_role'] == ' ') {
+                $data['new_role'] = '';
+            }
+
+            $newRole[] = $data['new_role'];
+
+            $user->setRoles($newRole);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user-roles');
+        }
+
+        return $this->render('/admin/edit_user.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
     }
 }

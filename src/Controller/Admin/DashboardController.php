7104d6b880bae19950f276a29a905352af48acbb
Dan Roberts <74026141+danroberts108@users.noreply.github.com>
2023-12-25 15:05:36 +0000

    Fitted easyadmin to project

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Movie;
+use App\Entity\Rating;
+use App\Entity\Report;
+use App\Entity\Review;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +19,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -30,6 +36,9 @@ class DashboardController extends AbstractDashboardController
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
         // return $this->render('some/path/my-dashboard.html.twig');
+
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(MovieCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -41,6 +50,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Movies', 'fa fa-film', Movie::class);
+        yield MenuItem::linkToCrud('Ratings', 'fa fa-scroll', Rating::class);
+        yield MenuItem::linkToCrud('Reports', 'fa fa-flag', Report::class);
+        yield MenuItem::linkToCrud('Reviews', 'fa fa-star', Review::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }

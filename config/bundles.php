f9a4f7b188c3b6a2cbd55fc611106c691e383742
danroberts108 <d.roberts15@edu.salford.ac.uk>
2024-02-29 17:21:29 +0000

    Configure json_login and jwt tokens

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
